f83f6d63bf1e513c202ac49a3b3c3150beeffcfe
jiny <hjin421@gmail.com>
2016-10-18 16:07:15 +0900

    main page edit
    main page edit

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var index= function(req, res) {
 	
 	res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 	res.write('<h1>Node.js Server</h1>');
-	res.write('<div><h2>Page Infomation</h2></div>');
-	res.write("<div><a href='/public/adduser.html'>/public/adduser.html : Add user.</a>");
-	res.write("<br><a href='/public/login.html'>/public/login.html : Login.</a>");
-	res.write("<br><a href='/public/listuser.html'>/public/listuser.html : Show user list.</a>");
-	res.write("<br><a href='/public/facebook.html'>/public/facebook.html : Add location & Show location list.</a></div>");
+	res.write('<div><h2>Page Inforomation</h2></div>');
+	res.write("<div><a href='/public/adduser.html'>Add user.</a>");
+	res.write("<br><a href='/public/login.html'>Login.</a>");
+	res.write("<br><a href='/public/listuser.html'>Show user list.</a>");
+	res.write("<br><a href='/public/facebook.html'>Add location & Show location list.</a></div>");
 	res.end();
 
 };
