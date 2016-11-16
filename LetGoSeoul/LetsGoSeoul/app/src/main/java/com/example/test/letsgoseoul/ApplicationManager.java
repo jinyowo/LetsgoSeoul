17e59a45a53bed48463700f39f5ca7816f0c7d5d
jiny <hjin421@gmail.com>
2016-11-16 20:31:31 +0900

    delete project 'LetGoSeoul' 잘못 올린 프로젝트 삭제

--- LetGoSeoul/LetsGoSeoul/app/src/main/java/com/example/test/letsgoseoul/ApplicationManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.letsgoseoul;
-
-import android.support.multidex.MultiDexApplication;
-
-/**
- * Created by jiny on 2016. 11. 15..
- */
-public class ApplicationManager extends MultiDexApplication {
-}
