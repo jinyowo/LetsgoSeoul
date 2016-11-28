965d56429a4c61ba46a25ea6c410d9d444f6cad7
HyunseonYang <vpqm@kookmin.ac.kr>
2016-11-28 22:01:33 +0900

    SPLASH를 런쳐액티비티로 설정 3초후 메인으로 이동 앱아이콘 설정

--- LetsGoSeoul/app/src/main/java/com/example/test/letsgoseoul/Splash.java
+++ LetsGoSeoul/app/src/main/java/com/example/test/letsgoseoul/Splash.java
@@ -1,12 +1,14 @@
 package com.example.test.letsgoseoul;
 import android.app.Activity;
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+
 /**
  * Created by jiny on 2016. 11. 28..
  */
-
-
 public class Splash extends Activity{
 
     @Override
@@ -15,15 +17,13 @@ public class Splash extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        Handler hd = new Handler();
-        hd.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                finish();       // 3 초후 이미지를 닫아버림
+        Handler hd = new Handler(){
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
             }
-        }, 3000);
+            };
+        hd.sendEmptyMessageDelayed(0,2000);
+                    }
     }
-
-
-}
